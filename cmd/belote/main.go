996eb1a42a13fc2fea37d371b718485050318a7f
delyan-kirov <delyan.k.kirov@gmail.com>
2024-03-19 16:33:33 +0200

    Started register an login pages

--- cmd/belote/main.go
+++ cmd/belote/main.go
@@ -1,11 +1,43 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/delyan-kirov/belote/internal/database"
-	_ "github.com/lib/pq"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// Test DB
 	database.Test()
+
+	// server
+
+	// Initialize Gin
+	fmt.Println("Starting gin")
+	router := gin.Default()
+
+	// Define a routeroute
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	router.Static("static", "./static")
+	router.LoadHTMLGlob("templates/*html")
+
+	// register page
+	router.POST("/register", func(ctx *gin.Context) {
+		username := ctx.PostForm("username")
+		password := ctx.PostForm("password")
+		ctx.String(
+			http.StatusOK,
+			"Registered successfully! Username: %s, Password: %s",
+			username,
+			password,
+		)
+	})
+
+	// run the server
+	router.Run(":8080")
 }
